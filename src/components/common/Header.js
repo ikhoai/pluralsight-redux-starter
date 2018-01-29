944898598a3268c15c549e8619bc0b53f4e9655c
Minh Dinh <khoai@Minhs-MacBook-Air.local>
2018-01-29 14:30:07 +0700

    Finisth #5

---
+++ src/components/common/Header.js
@@ -0,0 +1,17 @@
+import React, { PropTypes } from 'react'; 
+import { Link, IndexLink } from 'react-router';
+
+const Header = () => {
+  return (
+    <nav>
+      <IndexLink to="/" activeClassName="active">Home</IndexLink>
+      {" | "}
+      <IndexLink to="/about" activeClassName="active">About</IndexLink>
+      {" | "}
+      <IndexLink to="/course" activeClassName="active">Courses</IndexLink>
+    </nav>
+  );
+
+};
+
+export default Header;
